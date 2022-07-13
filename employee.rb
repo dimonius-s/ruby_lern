92cb2370b3c588536369db22299d441bc64ca9c6
dimonus <dimmonius-s@yandex.ru>
2022-07-13 07:33:09 -0400

    Initial commit

--- employee.rb
+++ employee.rb
@@ -28,13 +28,57 @@ class Employee
   end
 end
 
+class SalariiedEmployee < Employee
+  attr_reader :salary
+
+  def salary=(salary)
+    super(salary)
+  end
+
+  def initialize(name = 'Anonymous', salary = 0.0)
+    super(name)
+    self.salary = salary
+  end
+
+  def print_pay_stub
+    pay_for_period = (salary / 365.0) * 14
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
+
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wage=(hourly_wage)
+    raise "A hourly wage of #{hourly_wage} isn't valid!" if hourly_wage.negative?
+
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    raise "A hours per week of #{hours_per_week} of isn't valid!" if hours_per_week.negative?
+
+    @hours_per_week = hours_per_week
+  end
+
+  def initialize(name = 'Anonymous', salary = 0.0)
+    super(name)
+    self.salary = salary
+  end
+
+  def print_pay_stub
+    super(name, salary)
+    pay_for_period = hourly_wage * hours_per_week * 2
+    pay_for_period = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{pay_for_period}"
+  end
+
+end
+
+
 Employee.new('Jane Doe', 50_000).print_pay_stub
 Employee.new('Jane Doe').print_pay_stub
 Employee.new.print_pay_stub
 
-puts format('%12s | %s', 'Product', 'Cost in Cents')
-puts '-' * 30
-puts format('%12s | %2i', 'Stamps', 50)
-puts format('%12s | %2i', 'Paper Clips', 5)
-puts format('%12s | %2i', 'Tape', 99)
-puts format('%i', 3 / 4.0)
+
