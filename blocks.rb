2617d7cd9d34196be6f8b587c3820eef54cf97aa
dimonus <dimmonius-s@yandex.ru>
2022-07-21 03:04:18 -0400

    replace block.rb

--- blocks.rb
+++ blocks.rb
@@ -3,51 +3,46 @@
 def my_method(&my_block)
   puts "We're in the method, about to invoke your block!"
   my_block.call
-  puts "We're back in the method!"
+  puts "We're back in the block!"
 end
 
-my_method do
+my_method do 
   puts "We're in the block!"
 end
 
-my_method do
-  puts "It's a block party!"
+my_method {puts "We're in the block!"}
+
+def twice(&my_block)
+  puts "In the method, about call block!"
+  my_block.call
+  puts "Back in the method, about to call the block again!"
+  my_block.call
+  puts"Back in the method, about to call the block again!"
 end
 
-my_method do
-  puts 'Wooooo!'
+twice do
+  puts "Wooo!"
 end
 
-# def twice(&my_block)
-# puts "In the method, about to call the blcok!"
-# my_block.call
-# puts "Back in the method, about to call the block again!"
-# # my_block.call
-# puts "Back in the method, about to return!"
-# end
-#
+twice { puts "Wooo!" }
+
+def give(&my_block)
+  my_block.call("2 tutle doves", "1 partridge")
+end
+
+give do |present1, present2|
+  puts "My method gave to me..."
+  puts present1 ,present2
+end
+
 
 def twice
   yield
   yield
 end
 
-twice do
-  puts 'Woooooo!'
-end
-
-# def give(&my_block)
-#   my_block.call("2 turned doves", "1 partidge")
-# end
-#
-
 def give
-  yield '2 turned doves', '1 partidge'
-end
-
-give do |param_1, param_2|
-  puts 'My method gave to me ....'
-  puts param_1, param_2
+  yield "2 turtle doves", "1 partridge"
 end
 
 def run_block
@@ -55,13 +50,18 @@ def run_block
 end
 
 run_block do
-  puts 'do/end'
+  puts "do/end"
 end
 
-run_block { puts 'braces' }
+run_block { puts "braces" }
 
 def take_this
-  yield 'present'
+  yield "present"
+end
+
+take_this do |thing|
+  puts "do/end block got #{thing}"
 end
 
 take_this { |thing| puts "braces block got #{thing}" }
+
