4fbd7bde7161f043bb1b0807c873e02df546731d
dimonus <dimmonius-s@yandex.ru>
2022-07-21 11:56:17 -0400

    replace block.rb

--- blocks.rb
+++ blocks.rb
@@ -65,3 +65,41 @@ end
 
 take_this { |thing| puts "braces block got #{thing}" }
 
+def call_block(&block)
+  puts 1
+  block.call
+  puts 3
+end
+
+def call_twice
+  puts 1
+  yield
+  yield
+  puts 3
+end
+
+def pass_parameters_to_block
+  puts 1
+  yield 9, 3
+  puts 3
+end
+
+call_block do
+  puts 2
+end
+
+call_block { puts "two" }
+call_twice { puts 2 }
+call_twice do
+  puts "two"
+end
+
+pass_parameters_to_block do |param1, param2|
+  puts param1 + param2
+end
+
+pass_parameters_to_block do |param1, param2|
+  puts param1 / param2
+end
+
+
